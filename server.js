4b1bcd5c5230e20b88e5711e59fcaef89e2bd0b3
yangyang861115 <yangyang861115@gmail.com>
2016-04-03 11:35:35 -0400

    Pass connection to application

--- server.js
+++ server.js
@@ -3,5 +3,8 @@
  */
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+var db = mongoose.connect('mongodb://localhost/web-app-maker');
 
+require('./app/app.js')(db);
 app.listen(3000);
